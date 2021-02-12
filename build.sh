d1e765cb698715068e140bf946ae8d9693b2b2ff
iejMac <gig13omb@gmail.com>
2021-02-12 14:49:49 -0800

    removing -it

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 docker build -f Dockerfile -t plotjuggler:latest .
 
-docker run -it --rm --volume $PWD:/tmp/plotjuggler --workdir /tmp/plotjuggler plotjuggler:latest \
+docker run --rm --volume $PWD:/tmp/plotjuggler --workdir /tmp/plotjuggler plotjuggler:latest \
   /bin/bash -c "mkdir -p build && cd build && cmake .. && make -j8"
