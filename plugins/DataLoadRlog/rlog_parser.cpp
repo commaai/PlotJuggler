578d0733b38b71072e0dc432bb48405ec3f8be47
iejMac <gig13omb@gmail.com>
2021-02-17 16:52:23 -0800

    bracket fix

--- plugins/DataLoadRlog/rlog_parser.cpp
+++ plugins/DataLoadRlog/rlog_parser.cpp
@@ -12,23 +12,32 @@ bool RlogMessageParser::parseMessageImpl(const std::string& topic_name, capnp::D
   
   switch (value.getType()) 
   {
-    case capnp::DynamicValue::BOOL:
+    case capnp::DynamicValue::BOOL: 
+    {
       _data_series.pushBack({time_stamp, (double)value.as<bool>()});
-      break;
+    }
+    break;
 
-    case capnp::DynamicValue::INT:
+    case capnp::DynamicValue::INT: 
+    {
       _data_series.pushBack({time_stamp, (double)value.as<int64_t>()});
-      break;
+    }
+    break;
 
-    case capnp::DynamicValue::UINT:
+    case capnp::DynamicValue::UINT: 
+    {
       _data_series.pushBack({time_stamp, (double)value.as<uint64_t>()});
-      break;
+    }
+    break;
 
-    case capnp::DynamicValue::FLOAT:
+    case capnp::DynamicValue::FLOAT: 
+    {
       _data_series.pushBack({time_stamp, (double)value.as<double>()});
-      break;
+    }
+    break;
 
     case capnp::DynamicValue::LIST: 
+    {
       // TODO: Parse lists properly
       int i = 0;
       for(auto element : value.as<capnp::DynamicList>())
@@ -36,14 +45,18 @@ bool RlogMessageParser::parseMessageImpl(const std::string& topic_name, capnp::D
         parseMessageImpl(topic_name + '/' + std::to_string(i), element, time_stamp);
         i++;
       }
-      break;
+    }
+    break;
 
     case capnp::DynamicValue::ENUM: 
+    {
       auto enumValue = value.as<capnp::DynamicEnum>();
       _data_series.pushBack({time_stamp, (double)enumValue.getRaw()});
-      break;
+    }
+    break;
 
     case capnp::DynamicValue::STRUCT: 
+    {
       auto structValue = value.as<capnp::DynamicStruct>();
 
       for (auto field : structValue.getSchema().getFields()) 
@@ -54,11 +67,14 @@ bool RlogMessageParser::parseMessageImpl(const std::string& topic_name, capnp::D
           parseMessageImpl(topic_name + '/' + name, structValue.get(field), time_stamp); 
         }
       }
-      break;
+    }
+    break;
 
     default:
+    {
       // We currently don't support: DATA, ANY_POINTER, TEXT, CAPABILITIES, VOID
-      break;
+    }
+    break;
   }
   return true;
 }
