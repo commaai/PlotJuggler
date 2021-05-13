0f136ad4903cd0e5945b30c59863b6293b280d65
ShaneSmiskol <shane@smiskol.com>
2021-05-13 14:44:57 -0700

    this should get rlog loader source now

--- profiling/run-valgrind.sh
+++ profiling/run-valgrind.sh
@@ -1,12 +1,18 @@
 #!/bin/bash -e
 
+cd ../
+export LD_PROFILE_OUTPUT=$(pwd)
+cd profiling
+
+export LD_PROFILE=libDataLoadRlog.so
+rm -f $LD_PROFILE.profile
+
 # Run build.sh on your own if ../build exists but files have been updated
 [[ -d ../build ]] || (cd ../; sh build.sh)
 
 valgrind --tool=callgrind --dump-instr=yes --simulate-cache=yes --collect-jumps=yes \
-  --callgrind-out-file=./callgrind.out.pj ../build/bin/plotjuggler \
-  -d ./tmp0ic1nwvr.rlog
+  --callgrind-out-file=callgrind.out.pj ../build/bin/plotjuggler -d tmpaq3p1j9w.rlog
 
 # Close plotjuggler window manually after it loads log
 
-kcachegrind ./callgrind.out.pj
+kcachegrind callgrind.out.pj
