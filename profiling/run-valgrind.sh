c9afcedca0292d9e995f8981c9c7fa23ff8e0372
ShaneSmiskol <shane@smiskol.com>
2021-05-12 20:34:23 -0700

    fill out script. still need to figure out why it can't get source code of the rlog loader (sometimes)

--- profiling/run-valgrind.sh
+++ profiling/run-valgrind.sh
@@ -1,3 +1,12 @@
+#!/bin/bash -e
+
+# Run build.sh on your own if ../build exists but files have been updated
+[[ -d ../build ]] || (cd ../; sh build.sh)
+
 valgrind --tool=callgrind --dump-instr=yes --simulate-cache=yes --collect-jumps=yes \
   --callgrind-out-file=./callgrind.out.pj ../build/bin/plotjuggler \
   -d ./tmp0ic1nwvr.rlog
+
+# Close plotjuggler window manually after it loads log
+
+kcachegrind ./callgrind.out.pj
