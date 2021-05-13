b3b4af7aa43e9a4f0747ebb1b83f7f6848ec87d4
ShaneSmiskol <shane@smiskol.com>
2021-05-13 15:34:26 -0700

    slow, but works!

--- profiling/run-valgrind.sh
+++ profiling/run-valgrind.sh
@@ -1,9 +1,6 @@
 #!/bin/bash -e
 
-cd ../
 export LD_PROFILE_OUTPUT=$(pwd)
-cd profiling
-
 export LD_PROFILE=libDataLoadRlog.so
 rm -f $LD_PROFILE.profile
 
@@ -11,7 +8,9 @@ rm -f $LD_PROFILE.profile
 [[ -d ../build ]] || (cd ../; sh build.sh)
 
 valgrind --tool=callgrind --dump-instr=yes --simulate-cache=yes --collect-jumps=yes \
-  --callgrind-out-file=callgrind.out.pj ../build/bin/plotjuggler -d tmpaq3p1j9w.rlog
+  --callgrind-out-file=callgrind.out.pj `pwd`/../build/bin/plotjuggler -d `pwd`/tmpaq3p1j9w.rlog
+
+#$HOME/openpilot/PlotJuggler/build/bin/plotjuggler -d $HOME/openpilot/openpilot/tools/plotjuggler/tmpaq3p1j9w.rlog
 
 # Close plotjuggler window manually after it loads log
 
