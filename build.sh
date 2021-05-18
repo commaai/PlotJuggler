c825fa9034844b0d3c51b8280729c419f0c32429
ShaneSmiskol <shane@smiskol.com>
2021-05-18 14:31:55 -0700

    fix

--- build.sh
+++ build.sh
@@ -2,11 +2,14 @@
 
 cd 3rdparty/cereal
 docker pull docker.io/commaai/cereal:latest
-docker build --cache-from docker.io/commaai/cereal:latest -t cereal -f Dockerfile .  # todo: left off here!
+docker build --cache-from docker.io/commaai/cereal:latest -t cereal -f Dockerfile .
 docker run --rm --volume ${PWD}:/tmp/plotjuggler/3rdparty/cereal --workdir /tmp/plotjuggler/3rdparty/cereal --shm-size 1G --name cereal cereal \
-  /bin/sh -c "scons -c && scons -j8)"
+  /bin/sh -c "scons -c && scons -j8"
 
-cd ../../
+cd ../
+# todo: do opendbc here
+
+cd ../
 docker build -f Dockerfile -t plotjuggler:latest .
 
 docker run --rm --volume $PWD:/tmp/plotjuggler --workdir /tmp/plotjuggler plotjuggler:latest \
