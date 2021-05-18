0605a9a425bb158190b7cbe000429865a74241fc
ShaneSmiskol <shane@smiskol.com>
2021-05-18 15:10:58 -0700

    before resetting opendbc submodule

--- build.sh
+++ build.sh
@@ -1,13 +1,21 @@
 #!/bin/bash -e
 
 cd 3rdparty/cereal
+docker pull $(grep -ioP '(?<=^from)\s+\S+' Dockerfile)  # todo: required?
 docker pull docker.io/commaai/cereal:latest
 docker build --cache-from docker.io/commaai/cereal:latest -t cereal -f Dockerfile .
 docker run --rm --volume ${PWD}:/tmp/plotjuggler/3rdparty/cereal --workdir /tmp/plotjuggler/3rdparty/cereal --shm-size 1G --name cereal cereal \
-  /bin/sh -c "scons -c && scons -j8"
+  /bin/sh -c "scons -j8"
 
 cd ../opendbc
-# todo: do opendbc here
+docker pull $(grep -ioP '(?<=^from)\s+\S+' Dockerfile)  # todo: required?
+docker pull docker.io/commaai/opendbc:latest
+docker build --cache-from docker.io/commaai/opendbc:latest -t opendbc -f Dockerfile .
+docker run --rm --volume ${PWD}:/tmp/plotjuggler/3rdparty/opendbc --workdir /tmp/plotjuggler/3rdparty/opendbc --shm-size 1G --name opendbc opendbc \
+  /bin/sh -c "scons -j8"
+
+#docker run --rm --volume ${PWD}:/tmp/plotjuggler/3rdparty/cereal --workdir /tmp/plotjuggler/3rdparty/cereal --shm-size 1G --name cereal cereal \
+#  /bin/sh -c "scons -j8"
 
 cd ../../
 docker build -f Dockerfile -t plotjuggler:latest .
