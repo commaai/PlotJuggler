ecdee759b89d72c3959ff2b5965a0eda3e8fb8a3
ShaneSmiskol <shane@smiskol.com>
2021-05-19 20:44:30 -0700

    match procs

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 docker build -f Dockerfile -t plotjuggler:latest .
 
 docker run --rm --volume $PWD:/tmp/plotjuggler --workdir /tmp/plotjuggler plotjuggler:latest \
-  /bin/bash -c "(cd 3rdparty && cp opendbc/SConstruct . && cp -r opendbc/site_scons . && scons -j4) \
+  /bin/bash -c "(cd 3rdparty && cp opendbc/SConstruct . && cp -r opendbc/site_scons . && scons -j8) \
                 && mkdir -p build && cd build && cmake .. && make -j8"
