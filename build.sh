3ca7b637e6bb1906dbc45f0552bac1c67587e9d0
ShaneSmiskol <shane@smiskol.com>
2021-05-19 23:34:47 -0700

    build with scons in CMakeLists.txt

--- build.sh
+++ build.sh
@@ -3,5 +3,4 @@
 docker build -f Dockerfile -t plotjuggler:latest .
 
 docker run --rm --volume $PWD:/tmp/plotjuggler --workdir /tmp/plotjuggler plotjuggler:latest \
-  /bin/bash -c "(cd 3rdparty && cp opendbc/SConstruct . && cp -r opendbc/site_scons . && scons -j8) \
-                && mkdir -p build && cd build && cmake .. && make -j8"
+  /bin/bash -c "mkdir -p build && cd build && cmake .. && make -j8"
